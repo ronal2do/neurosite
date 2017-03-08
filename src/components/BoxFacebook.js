dff81a4a1d4679b077fd04b0df7ed03899442d50
Ronaldo <ronaldo@SOTMAC16.local>
2017-03-08 12:37:42 -0300

    facebook token

--- src/components/BoxFacebook.js
+++ src/components/BoxFacebook.js
@@ -35,7 +35,9 @@ const foot = css({
 class BoxFacebook extends Component {
   state = {
     post: '',
-    access_token: 'EAAa7Pb6FkLgBABAz86U3ZBFoEF3Rs4HpmSbdBs0zscPgvUqAeXSofq80orTV6RWSpJYbpXHiFxd9OY5nyH7iVKfX4ZAmiPz71Rmx1DCvN7qRiPYPsFpkUrTRQ2VE7iJkDfvDkFFFMv8N7WJZAm4AQOhM0ryqvYZD',
+    access_token: 'EAACEdEose0cBAAaPZCTnQZB4ZAO1HsgFXIYRJeIqyms9APeQuhfT2zg6rguZBjMAp82LmOeYFKTUmKyiG9tY9FUZCPop6ooUnJF9cpnYkTsu429S3pWxIvinwFGntnV0WWIXJZCUZBZCbYvK2O3cZAZCCUAx7omiKSin5HXc0k2u5fNtNlWZBfZCO99ZBRHso2urDSB8ZD',
+    // access_token: '2135275936698660|KnT3NpVx-9n7MGSIANzVT32JCho',
+    // access_token: 'EAACEdEose0cBAOitveAXu9tLsHWS3beK4oq8fxLPrZBV2n15u3lZCSKNRHZANksTzVZBhQTLKLSSvLxN8PTMSuQEfcHWBhAWF3gYKBCzKCp6ZALqfVGGrowK2kgnnG1tRw05zdVOv0lfWv66sbe6lYdNPO9WR5Y2amerMa9HzlliZBIabkw9nZBLiC0MjxNMHgZD',
   }
 
   componentDidMount() {
@@ -47,8 +49,9 @@ class BoxFacebook extends Component {
 
     graph.setAccessToken(this.state.access_token);
 
-    graph.get('neuroedu.co/posts', {limit: 1}, (err, res) => {
-      if (res.data[0].message) {
+    graph.get('/neuroedu.coBR/posts?fields=message,picture,created_time,story,description,link', {limit:10}, (err, res) => {
+      // console.log(res.data[0]);
+      if (res.data[0]) {
         const rp = res.data[0];
          this.setState({
              post: rp
@@ -66,17 +69,24 @@ class BoxFacebook extends Component {
           <img src={commma} alt="" {...css({marginTop: '-5px', padding: '0 50px'})}/>
         </div>
         <h1 style={{color: 'white'}}>{moment(post.created_time).fromNow()}</h1>
-        <p {...css({padding: '30px 50px'})}>
-          {post.message}
-        </p>
-        <div className={foot}>
-          <div {...css({width: '50%',display: 'flex', justifyContent: 'center' ,padding: '30px 0px', borderRight: '1px solid white',})}>
-            <i className="fa fa-facebook"></i>
-          </div>
-          <div {...css({width: '50%',display: 'flex', justifyContent: 'center' ,padding: '30px 0px'})}>
-            <i className="fa fa-thumbs-up"></i>
-          </div>
+        <div {...css({ display: 'flex'})}>
+          <p {...css({padding: '20px 50px'})}>
+            { post.message ? post.message : post.description}
+          </p>
         </div>
+
+          <div className={foot}>
+            <div {...css({width: '50%',display: 'flex', justifyContent: 'center' ,padding: '30px 0px', borderRight: '1px solid white',})}>
+              <a href={ post.link } target="_blank">
+                <i className="fa fa-facebook"></i>
+              </a>
+            </div>
+            <div {...css({width: '50%',display: 'flex', justifyContent: 'center' ,padding: '30px 0px'})}>
+              <a href={ post.link } target="_blank">
+                <i className="fa fa-thumbs-up">{post.admin_creator}</i>
+              </a>
+            </div>
+          </div>
       </section>
     );
   }
--- src/front/Portal.js
+++ src/front/Portal.js
@@ -4,18 +4,18 @@ import Globals from '../utils/Globals';
 
 import Banner from '../components/Banner';
 import Section from '../components/Section';
-// import Container from '../components/Container';
+import Container from '../components/Container';
 import Search from '../components/Search';
 // import VideoFeature from '../components/VideoFeature';
 import ChannelFeature from '../components/ChannelFeature';
 import Lessons from '../components/Lessons';
-// import BoxFacebook from '../components/BoxFacebook';
-// import BoxYoutube from '../components/BoxYoutube';
+import BoxFacebook from '../components/BoxFacebook';
+import BoxYoutube from '../components/BoxYoutube';
 import PortalForm from '../components/PortalForm';
 import Footer from '../components/Footer';
 
 import portal from '../media/images/portal.png';
-// const transmission = 'ylXShcoF0xA';
+const transmission = 'ylXShcoF0xA';
 // import transmission from '../media/images/transmission.png';
 import inscrevase from '../media/images/inscrevase.png';
 
@@ -28,7 +28,7 @@ class Portal extends Component {
           <Search />
         </Section>
 
-        {/* <Section color="#3b5999" nopadding>
+        <Section color="#3b5999" nopadding>
           <Container>
             <BoxFacebook />
             <Section color="rgb(63, 67, 71)"
@@ -37,7 +37,7 @@ class Portal extends Component {
             <BoxYoutube link={`https://www.youtube.com/watch?v=${transmission}`}/>
             </Section>
           </Container>
-        </Section> */}
+        </Section>
 
         <Section color="#1e2325">
           <ChannelFeature />
